0686d857ce70f0367964581e7cddc629a04782c3
An041003 <binhan214981@gmail.com>
2025-05-28 23:22:32 +0700

    Back

--- backend/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile
-
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = [
-        'user', 'full_name', 'birthdate', 'gender', 'height', 'weight', 'waist', 'neck', 'hip', 'bmi', 'body_fat', 'updated_at'
-    ]
--- frontend/src/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-    return (
-        <div className="text-center mt-10">
-            <h1 className="text-3xl font-bold">Welcome to FoodPro!</h1>
-        </div>
-    );
-}
