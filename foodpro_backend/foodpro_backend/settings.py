a90b2fef547ddc8676759b6161234dcfd0752f43
An041003 <binhan214981@gmail.com>
2025-05-28 19:14:07 +0700

    .

--- foodpro_backend/foodpro_backend/settings.py
+++ foodpro_backend/foodpro_backend/settings.py
@@ -111,8 +111,12 @@ if DATABASE_URL:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / "db.sqlite3",
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'mydb',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'localhost',  
+            'PORT': '5173',
         }
     }
 
